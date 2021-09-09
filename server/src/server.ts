4fee08c2a256a888db5ca982cb64748cd7e60ffd
Jaqueline <jaquelineribeiro015@gmail.com>
2021-09-09 14:55:20 -0300

    Alterações no código

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
+
+    //json
+
+    response.json( [
+        'Robson',
+        'Claudio',
+        'Ana'
+    ] );
 });
 
 app.listen(3333); 
